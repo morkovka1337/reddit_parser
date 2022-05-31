f1a3ee5d9e51f188a612bfbb021b71e76a2c5407
morkovka1337 <vladimir.loginov@intel.com>
2022-05-31 12:23:33 +0300

    initial

---
+++ parse_redding_multiprocessing.py
@@ -0,0 +1,104 @@
+from re import sub
+import praw
+import pandas as pd
+import multiprocessing as mp
+import itertools
+from praw.models import MoreComments
+import os
+
+from cridentials import client_id, client_secret
+
+CHECK_CORRECTNESS=False
+CORE_MULTIPLICATOR = 4
+
+def parse_subreddit_filter(subr, filter):
+
+    reddit = praw.Reddit(client_id=client_id,
+                         client_secret=client_secret,
+                         user_agent='Reddit stocks analysis big data 2022',
+                         # password='',
+                         # username=''
+                         )
+    subreddit = reddit.subreddit(subr)
+    submissions = getattr(subreddit, filter)(limit=None)
+    df = pd.DataFrame(columns=['Title', 'Post text', 'Comments'])
+    i = 0
+    print(f'Processing {subr}.{filter}')
+    for submission in submissions:
+        submission.comments.replace_more(limit=100)
+        for comment in submission.comments:
+            if isinstance(comment, MoreComments):
+                continue
+            comm = comment.list()
+
+            df.loc[i] = [submission.title] + \
+                [submission.selftext] + [comm.body]
+            i += 1
+
+    df.to_json(f'{subr}_{filter}.json')
+
+def check_correctness(subr, filter):
+
+    reddit = praw.Reddit(client_id='43CpG446wyP-WiKGtRZmZw',
+                         client_secret='yORJ6KQtTYYOhhLpLesUrVRzxyMX9w',
+                         user_agent='Reddit stocks analysis big data 2022',
+                         # password='',
+                         # username=''
+                         )
+    subreddit = reddit.subreddit(subr)
+    submissions = list(getattr(subreddit, filter)(limit=10))
+    assert submissions is not None
+    print(f'{subr}.{filter} is correct!')
+
+if __name__ == '__main__':
+
+
+    subreddits = [
+        'wallstreetbets',
+        'stocks',
+        'pennystocks',
+        'investing',
+        'ValueInvesting',
+        'investing_discussion',
+        'stonks',
+        'shroomstocks',
+        'Wallstreetbetsnew',
+        'StockMarket',
+        'options',
+        'RobinHood',
+        'RobinHoodPennyStocks',
+        'weedstocks',
+        'smallstreetbets',
+        'SecurityAnalysis',
+        'CanadianInvestor',
+        'SPACs',
+        'InvestmentClub',
+        'GreenInvestor',
+        'EducatedInvesting',
+        'UndervaluedStonks',
+        'Canadapennystocks',
+        'traders',
+        'RichTogether',
+        'UnderValuedStocks',
+    ]
+
+    filters = [
+        'top',
+        'hot',
+        'controversial',
+        'rising',
+        'random_rising',
+        'gilded',
+        'new',
+    ]
+    subreddits_filters = list(itertools.product(*[subreddits, filters]))
+    inputs = [subreddits_filters[i] for i in range(len(subreddits_filters)) if not os.path.exists(f'{subreddits_filters[i][0]}_{subreddits_filters[i][1]}.json')]
+    all_inputs = [subreddits_filters[i] for i in range(len(subreddits_filters))]
+    if CHECK_CORRECTNESS:
+        for elem in subreddits_filters:
+            check_correctness(*elem)
+
+    pool = mp.Pool(processes=mp.cpu_count() * CORE_MULTIPLICATOR)
+    result = pool.starmap(parse_subreddit_filter, inputs)
+
+
